fa8ad5d292086116716f1f270fc11ee8754da402
Jameson Collins <jameson.collins@gmail.com>
2024-12-26 13:45:07 -0500

    aming

--- run.py
+++ run.py
@@ -160,7 +160,7 @@ class PingCommand(Command):
         groupId = None
         try:
             groupId = c.message.group
-            groupId.replace("\\", "\\\\")
+            groupId.replace("/", "-&-")
         except:
             pass
         msg = c.message.text
