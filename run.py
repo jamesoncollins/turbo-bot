5135ca2fdc88c070833f8c2c7b603a9383d103d2
Jameson Collins <jameson.collins@gmail.com>
2024-12-27 09:26:01 -0500

    diag

--- run.py
+++ run.py
@@ -14,22 +14,24 @@ class PingCommand(Command):
             groupId = groupId.replace("/", "-")
         except:
             pass
+
         msg = c.message.text
-        #print(vars(c.message))
         sourceName = c.message.raw_message["envelope"]["sourceName"]
         sourceNumber = c.message.raw_message["envelope"]["sourceNumber"]
-        #print(msg)
+
         if msg == "Ping":
+            print("is ping")
             await c.send("Pong")
         elif (url := is_reddit_domain(msg)):
             print("is reddit url")
             if (fname:=download_reddit_video(url)):
-                await c.reply("is reddit url " + url, base64_attachments=[file_to_base64(fname)])
+                await c.reply("Reddit URL: " + url, base64_attachments=[file_to_base64(fname)])
         elif (url := is_youtube_domain(msg)):
-            print("YouTube URL: " + url)
+            print("is youtube url " + url)
             if (fname:=download_youtube_video(url)):
                 await c.reply("YouTube URL: " + url, base64_attachments=[file_to_base64(fname)])
         elif "instagram.com" in msg:
+            print("is insta")
             url = extract_url(msg, "instagram.com")
             if url:
                 fname = download_insta(url)
@@ -40,26 +42,32 @@ class PingCommand(Command):
             else:
                 await c.send("failed to get url")
         elif (tickers := find_ticker(msg)):
+            print("is ticker")
             stock = yf.Ticker(tickers[0])
             recent_data = yf.download(tickers, period="1y")            
             recent_data['Close'].plot()
             plt.savefig("//tmp//tick.png")
             await c.reply("plot", base64_attachments=[file_to_base64("//tmp//tick.png")])  
         elif "#gpt" in msg:
+            print("is gpt")
             query =  msg.replace("#gpt", "") 
             res = submit_gpt(query)
             await c.reply(res)
         elif "#mmw" == msg and groupId:
+            print("is mmw")
             mmw = print_file(f"mmw{groupId}.txt")
             await c.send("History: \n" + mmw)            
         elif "#mmw" in msg and groupId:
+            print("is mmw")
             mmw = sourceName + "(" + sourceNumber + "): " + msg
             await c.send(mmw)
             with open(f"mmw{groupId}.txt", "a") as file:
                 file.write(mmw+"\n")
         elif msg == "#":
+            print("is hash")
             await c.send("I am here.")            
         elif msg == "#turboboot":
+            print("is reboot")
             await c.send("turbobot rebooting...")
             quit()          
         return
