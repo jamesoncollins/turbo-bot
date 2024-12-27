e4e12dccd037821e1e293603f887d85637b5ae1a
Jameson Collins <jameson.collins@gmail.com>
2024-12-27 18:41:43 -0500

    f

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define variables
-PYTHON_SCRIPT="run.py"   # Replace with the name of your Python script
+PYTHON_SCRIPT="your_script.py"   # Replace with the name of your Python script
 CHECK_INTERVAL=60                # Time interval to check the repo for updates (in seconds)
 
 # Function to run the Python script
@@ -9,7 +9,11 @@ run_python_script() {
     python3 "$PYTHON_SCRIPT" &
     PYTHON_PID=$!
     wait $PYTHON_PID
-    exit 1  # Exit the script if the Python script terminates
+    EXIT_CODE=$?
+    if [ $EXIT_CODE -ne 0 ]; then
+        echo "Python script exited with code $EXIT_CODE. Exiting bash script."
+        exit $EXIT_CODE
+    fi
 }
 
 # Trap to clean up background processes on exit
@@ -31,7 +35,7 @@ while :; do
         git pull > /dev/null 2>&1
 
         echo "Relaunching the Python script..."
-        kill "$PYTHON_PID"
+        kill -0 "$PYTHON_PID" 2>/dev/null && kill "$PYTHON_PID"
         run_python_script &
     fi
 
