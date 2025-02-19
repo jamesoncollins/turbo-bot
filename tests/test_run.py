e89c39e19fe78a9c5362d3d82f9284d092a655dc
Jameson Collins <jameson.collins@gmail.com>
2025-02-19 18:09:10 -0500

    that commit only worked on windows

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,8 +1,9 @@
 # allow importing of local signalbot
 import sys
 import os
-current_dir = __file__
-path_to_append = os.path.join(current_dir, "../../signalbot_local/")
+current_dir = os.path.abspath(__file__)
+current_dir = os.path.dirname(current_dir)
+path_to_append = os.path.join(current_dir, "../signalbot_local/")
 if os.path.exists(path_to_append):
     sys.path.append(path_to_append)
     print(f"Appended {path_to_append} to sys.path")
